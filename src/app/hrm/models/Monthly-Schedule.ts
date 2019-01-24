d1e40b6ca4a541ceb3bce955452964f77bf4d5bf
heka lemma <heka.big@gmail.com>
2019-01-23 23:27:14 -0800

    worked on hrm model monthly schedule

---
+++ src/app/hrm/models/Monthly-Schedule.ts
@@ -0,0 +1,28 @@
+
+export class MonthlySchedule implements Deserializable {
+
+  public id: number;
+  public dayOfMonth: number;
+  public scheduleId: Schedule[];
+
+
+  deserialize(single: any) {
+    if (single == null) return null;
+
+    this.scheduleId = new Schedule().deserializeArray(<any>single.scheduleId);
+    delete single.scheduleId;
+
+    //noinspection TypeScriptUnresolvedFunction
+    Object.assign(this, single);
+    return this;
+  }
+
+  deserializeArray(multi: any) {
+    if (multi == null) return [];
+    let arrayOfMe = [];
+    for (let one of multi) {
+      arrayOfMe.push(new MonthlySchedule().deserialize(one));
+    }
+    return arrayOfMe;
+  }
+}
