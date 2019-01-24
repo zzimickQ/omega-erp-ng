ea9bf5c411a6882d6794a66cd05b69f3efa9e805
heka lemma <heka.big@gmail.com>
2019-01-23 23:46:09 -0800

    worked on hrm model Performance Evaluation

---
+++ src/app/hrm/models/Performance-Evaluation.ts
@@ -0,0 +1,36 @@
+import {Employee} from "./Employee";
+
+export class PerformanceEvaluation implements Deserializable {
+
+  public id: number;
+  public remark: string;
+  public grade:number;
+  public evaluationDate:Date;
+  public evaluatingEmployee:Employee;
+  public employee:Employee;
+
+
+  deserialize(single: any) {
+    if (single == null) return null;
+
+    this.evaluatingEmployee = new Employee().deserialize(<any>single.evaluatingEmployee);
+    delete single.evaluatingEmployee;
+
+    this.employee = new Employee().deserialize(<any>single.employee);
+    delete single.employee;
+
+
+    //noinspection TypeScriptUnresolvedFunction
+    Object.assign(this, single);
+    return this;
+  }
+
+  deserializeArray(multi: any) {
+    if (multi == null) return [];
+    let arrayOfMe = [];
+    for (let one of multi) {
+      arrayOfMe.push(new PerformanceEvaluation().deserialize(one));
+    }
+    return arrayOfMe;
+  }
+}
